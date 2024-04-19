328fda9d16513ebf9accfe090afd6f252589d5a0
Wasealex <wasealex27@gmail.com>
2024-04-19 15:41:43 +0300

    4 number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application: listening on 0.0.0.0, port 5000
+Routes:"/" and display “Hello HBNB!” strict_slashes=False
+"""
+from flask import Flask
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_HBNB():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def display_HBNB():
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def C_is_fun(text):
+    return f"C {escape(text.replace('_', ' '))}"
+
+
+@app.route('/python/', defaults={'text': 'is cool'})
+@app.route("/python/<text>")
+def python_is_cool(text):
+    return f"Python {escape(text.replace('_', ' ') or 'is cool')}"
+
+
+@app.route('/number/<int:n>')
+def number_let(n):
+    return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    Flask.run(app)
